db3668dc60c550396a6f3086121bd7b6b3917129
MuhamadAgmaldo <muhammadagmaldo05@gmail.com>
2022-04-02 14:13:21 +0700

    menambahkan tombol selected

--- src/component/CardSong.js
+++ src/component/CardSong.js
@@ -1,18 +1,20 @@
-import './CardSong.css';
+import "./CardSong.css";
 
 function CardSong(props) {
-    return (
-        <div className="Card">
-            <img src={props.url} alt={props.alt} />
-            <div className="text-component" >
-                <p>{props.name}</p>
-                <p>{props.artists}</p>
-            </div>
-            <div className='button-component'>
-                <button type="button" onclick="alert(Hello World!')">Select</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Card">
+      <img src={props.url} alt={props.alt}></img>
+      <div className="text-component">
+        <p className="album">{props.albumName}</p>
+        <p>{props.artistName}</p>
+      </div>
+      <div className="button-component">
+        <button className="buttonSelect" type="button" onClick={() => props.onClick (props.isSelected)}>
+          {props.nameOfButton}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CardSong;
